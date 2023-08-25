4cee19af843379d693adae0e6091f8cc4cf74f35
TRIPAR12 <rashmi.tripathi@pfizer.com>
2023-08-25 11:00:40 +0000

    Initial Push

---
+++ webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function (AppComponent) {
+	return AppComponent.extend("sap.gts.fiori.misccshipments.Component", {
+		metadata: {
+			"manifest": "json"
+		}
+	});
+});
